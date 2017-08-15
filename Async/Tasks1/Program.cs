a9a2191ce8d70db17b02de3815cc548fb4358b6b
Andrew <andrewdunn50@gmail.com>
2017-08-16 05:46:25 +1000

    Updaint task example.

---
+++ Async/Tasks1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = new TaskSample();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sample.SimpleTask();
+            }
+
+            Console.Read();
+        }
+
+    }
+}
---
+++ Async/Tasks1/TaskSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tasks1
+{
+    class TaskSample
+    {
+        private int _taskCounter = 0;
+        private object lockObject = new object();
+
+        internal void SimpleTask()
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                lock (lockObject)
+                {
+                    _taskCounter++;
+                }
+
+                int taskNumber = _taskCounter;
+
+                for (int i = 0; i < int.MaxValue; i++) { }
+
+                return taskNumber;
+            });
+
+            //Continuation - fire task after 'task' is finished
+            var continueTask = task.ContinueWith((antecedent) => //antecendent - task that fired this task (parent)
+            {
+                Console.WriteLine($"{Environment.NewLine}Task {antecedent.Result} Complete");
+            }); 
+        }
+
+    }
+}
